ed8f8c13025261ccf712e838b53f94e527cf2a3e
jeongjiwon <jungkt0x@naver.com>
2023-10-13 17:09:33 +0900

    feat: 회사, 사용자 초기데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user values(29, 1, '01012345678', '안녕하세요.!!', '정지원', '백엔드')
+insert into user values(27, 2, '01022349876', '프론트엔드 지원합니다!', '김티드', '프론트엔드')
+insert into user values(25, 3, '01012312312', 'IOS 지원합니다!', '홍길동', 'IOS')
+
+insert into company values(1, '원티드', '대한민국', '서울')
+insert into company values(2, '카카오', '대한민국', '분당')
+insert into company values(3, '네이버', '대한민국', '판교')
+
